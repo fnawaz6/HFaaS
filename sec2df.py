25a7450ad75dca2e54427500c0d1fb762c9ab0b3
alan khosro <alan.khosro@gmail.com>
2017-11-17 20:21:14 -0800

    New function: updateSECs added and improvemnets to Read_sec

--- sec2df.py
+++ sec2df.py
@@ -1,19 +1,20 @@
+#To read data sources and save
 import pandas as pd
-cik2ticker = pd.read_csv ("cik2ticker.csv")[['cik', 'ticker']]
 #################################################################
-''' read financial data submitted to SEC by corporations for each quarters
-and acleans and re-creates 10-K and 10-Q datasets'''
-def read_sec(path="C:\\Github\\HFaaS\\temp\\2017q3\\", cik2ticker=None, encoding="latin"): #-> pd.DataFrame
+def read_sec(path, cik2ticker, encoding="latin"): #-> pd.DataFrame
+    ''' reads financial data submitted to SEC by firms for each quarter
+    and re-creates 10-K and 10-Q datasets'''
     ##### read num, pre, sub, cik2ticker; 
     num = (
         pd.read_table(path+"num.txt", encoding=encoding) 
-        .query('value.notnull()')
+        .query('value.notnull() & value != 0')
         #.query('coreg.notnull()') # filter submitted as a coregistrant
         #num[num['ddate']<=int(dt.now().strftime('%Y%m%d'))] # filter values for future date
     )
     pre = (
         pd.read_table(path+"pre.txt", encoding=encoding)
-        #.query(('stmt==["BS", "IS", "CF", "EQ", "CI"]')
+        .query('plabel.isnull()')
+        #.query('stmt==["BS", "IS", "CF", "EQ", "CI"]')
     )
     #tag = pd.read_table (secPath+"tag.txt", encoding=encode)
     sub = (
@@ -23,38 +24,59 @@ def read_sec(path="C:\\Github\\HFaaS\\temp\\2017q3\\", cik2ticker=None, encoding
     )
     ##### merge all to df; create stamp and fiscal; select columns; 
     df = (num 
-        .merge(pre, on=["adsh", "tag", "version"], how="left") 
-        .merge(sub, on="adsh", how="left") 
-        .rename(columns={'plabel':'item', 'ddate':'date'}) 
-        .assign(fisQtr = lambda x: fqtr(x.date, x.fye)) 
+        .merge(pre, on=["adsh", "tag", "version"], how="inner") 
+        .merge(sub, on="adsh", how="inner") 
+        .assign(fisQtr = lambda x: fqtr(x.ddate, x.fye)) 
     )
     ##### create and concat 10-K and 10-Q reports; return dataframe 
     Q10 = (df
         .query('qtrs == [0,1]')
         .assign(report="10-Q")
-        .assign(stamp = lambda x: year_qtr(x.date))
+        .assign(stamp = lambda x: year_qtr(x.ddate))
     )
     K10 = (df
         .query('qtrs == [0,4] & fisQtr == 4')
         .assign(report="10-K")
-        .assign(stamp = lambda x: year_qtr(x.date))
+        .assign(stamp = lambda x: year_qtr(x.ddate))
     )
     return (
         pd.concat([K10, Q10], join='outer')
-        .filter(['ticker', 'report', 'stmt', 'item', 'uom', 'stamp', 'value', 'cik', 'tag', 'fisqtr', 'date', 'qtrs'])
+        .rename(columns={'plabel':'item', 'ddate':'date'}) 
+        .filter(['ticker', 'report', 'stmt', 'item', 'stamp', 'value', 'uom', 'cik', 'tag', 'fisqtr', 'date', 'qtrs'])
     )
 #################################################################
-''' Converts dates of int type to year-quarter integer 20170931 -> 20173'''
- def year_qtr (dates, digits=8):
+def year_qtr(dates, digits=8):
+    ''' Converts dates of int type to year-quarter integer 20170931 -> 20173'''
     year = dates // 10**(digits-4)
     month = dates // 10**(digits-6) % 100
     qtr = (month-1) // 3 + 1
     return year*10+qtr
 #################################################################
-'''calculates the fiscal quarter for each date given the endyear'''
 def fqtr (dates, fYearEnd):
+    ''' Calculates the fiscal quarter for each date given the endyear'''
     month = dates // 100 % 100
     fmonth = fYearEnd // 100
     fquarter = (11 + month - fmonth) // 3 % 4 + 1
     return fquarter
+#################################################################
+def updateSECs (df=pd.DataFrame(), path="C:/Github/HFaaS/Data/SEC/", keep='last'):
+    ''' updates existing df of sec data (10-Q and 10_K) from zip files in path
+    then archives the added zip files. If you want to add older files use keep="first" '''
+    import zipfile, os, shutil
+    newSecZipFiles = [f for f in sorted(os.listdir(path)) if f.endswith(".zip")]
+    for zip in newSecZipFiles:
+        try:
+            with zipfile.ZipFile(zip) as z:
+                z.extractall(path+"temp/")
+            new = read_sec(path+"temp/", cik2ticker)
+            df = df.append(new)
+            df = df.drop_duplicates(subset=['cik', 'report', 'stmt', 'item', 'stamp'], keep=keep)
+            shutil.rmtree(path+"temp/")
+            shutil.move(path+zip, path+'Archive/'+zip)
+        except Exception as e:
+            print(zip, e)
+    return df
+###################################################################
+
+
 
