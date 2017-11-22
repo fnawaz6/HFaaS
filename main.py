ff6e4369fefa88df4b7475f0251e7b9d1c842f8d
alan khosro <alan.khosro@gmail.com>
2017-11-22 01:06:26 -0800

    sample data is added for income statement (current task), sec data, and mapping sec 2 google

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os, pandas as pd, numpy as np, pickle, zipfile, shutil, sys
-os.chdir("C:\\Github\\HFaaS\\GitHub\\")
+os.chdir("C:\\Github\\HFaaS\\DevCodes\\sample data\\")
 sys.path.append("C:\\Github\\HFaaS\\GitHub\\")
 import sec2df
 
@@ -9,7 +9,11 @@ sec = updateSECs()
 
 a = read_sec ("C:\\Github\\HFaaS\\temp\\temp\\", cik2ticker)
 
+a = pd.read_csv("C:\\Github\\HFaaS\\Output\\sec2009_2017q3.csv", encoding='latin', nrows=10)
+
 b = download_Financials_From_Google("MSFT", "NASDAQ")
+
+c = pd.read_csv("C:\\Github\\HFaaS\\Output\\std2items.tsv", sep="\t", nrows=10)
 
 ######################### END of main program
 
