e87dad4190c6f4158027b4056cdbd0f0fb1a26fb
alan khosro <alan.khosro@gmail.com>
2017-11-22 00:57:16 -0800

    secStmtFromExcel: three functions added: to_date, year_qtr, to_qtrs, and read IS is changed to have all variables

--- secStatementsFromExcel.py
+++ secStatementsFromExcel.py
@@ -1,17 +1,17 @@
-import pandas as pd 
+import pandas as pd
+from dateutil import parser
  
-url_to_excel_file_of_financial_statements = "https://www.sec.gov/Archives/edgar/data/1416697/000112785517000297/Financial_Report.xlsx"
+url = url_to_excel_file_of_financial_statements = "https://www.sec.gov/Archives/edgar/data/1416697/000112785517000297/Financial_Report.xlsx"
 
 ######### Read Income Statement
 IS = pd.read_excel(url, sheet_name=3, index_col=0, header=[0,1])
-pd.to_datetime(IS.columns.levels[1])
-a = IS.stack(dropna=True).reset_index()
-a.columns = ["item", "date", "value"]
-a.value=pd.to_numeric(a.value, errors='coerce', downcast='float')
-a = a.dropna()
-pd.to_datetime(a.date)
-a.assign(report, stmt, qtrs, stamp, date)
-
+a = IS.stack(dropna=True, level=[0,1]).reset_index()
+a.columns = ["item", "qtrs", "date", "value"]
+a['value'] = pd.to_numeric(a.value, errors='coerce', downcast='float')
+a['date'] = to_date (a['date'])
+a['stamp'] = year_qtr(a['date'])
+a['qtrs'] = to_qtrs(a['qtrs'])
+a = a.assign(stmt="IS", ticker="xxx", report="xxx", uom="USD", cik="xxx", tag="xxx", fisqtr="xxx")
 
 ########### Real all financials
 url_to_all_filing_by_form_and_quarter = "https://www.sec.gov/Archives/edgar/full-index/2017/QTR4/form.idx"
@@ -24,3 +24,31 @@ a['url'] = "https://www.sec.gov/Archives/"+a['url']
 
 for i in a:
     read excel tables and stack them and clean them
+
+###############################################
+def to_qtrs (periods): # -> quarters as a pd.Series 
+    ''' extracts the number of quarters that a period covers,
+    for intance "9 months ends" to 3 qtrs'''
+    import re
+    conversion = pd.Series([.25, 1, 3, 13, 91], index=["yea", "qua", "mon", "wee", "day"], name="perQtr")
+    periods = ( periods
+        .str.lower().str.extract('(\d) *(\w{3})', expand=True)
+        .rename(columns={0:"duration", 1:"unit"})
+        .join(conversion, on="unit")
+    )
+    qtrs = periods['duration'].astype('float').divide(periods['perQtr'])
+    return (qtrs.round())
+###############################################
+def to_date (dates):
+    ''' converts input string dates to our format of dates that is integer '''
+    return (
+        pd.to_datetime(dates, infer_datetime_format=True, errors='coerce')
+        .dt.strftime("%Y%m%d")
+        .astype(int)
+    )
+###############################################
+def year_qtr (dates):
+    ''' converts input string dates to our year-quarter format of stamp as integer'''
+    dates = pd.to_datetime(dates, infer_datetime_format=True, errors='coerce')
+    return (dates.dt.year*10 + dates.dt.quarter)
+###############################################
